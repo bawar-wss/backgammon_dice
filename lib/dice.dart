a2a27569477c5643259b537daaf440f406b14583
Bawar <bawar.wss@gmail.com>
2023-02-25 12:56:08 -0800

    Adding math library

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNum = 1;
+  int rightNum = 1;
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftNum = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('assets/images/dice$leftNum.png'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightNum = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('assets/images/dice$rightNum.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.cyan.shade800,
         appBar: AppBar(
